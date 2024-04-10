57356325a5d9215b1ca2b9396aa16a7fbd58f5ef
rileyeward <rileyward@ufl.edu>
2024-04-10 17:28:15 -0400

    Main menu and encoder function

---
+++ red_newlab9.py
@@ -0,0 +1,27 @@
+def encoder(number): #Riley Ward
+    encoded = ''
+    for char in number:
+        x = int(char) + 3
+        x = str(x)
+        encoded += x
+    return encoded
+
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit\n")
+
+    option = int(input("Please enter an option: "))
+
+    if option == 1:
+        password = input("Please enter password to encode: ")
+        new_password = encoder(password)
+        print("Password has been encoded and stored!\n")
+
+    elif option == 2:
+        print("The encoded pass word is " + str(new_password) + ", and the original password is " + str() + ".\n")
+
+    elif option == 3:
+        break
